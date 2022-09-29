4922ea558cc774cfedad1cdb1e505781764def70
MiyazakiHiroaki <h12.miyazakihiroaki@gmail.com>
2022-09-29 21:49:44 +0900

    test

--- shift_managementproject/settings.py
+++ shift_managementproject/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ####追加###
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'shift_managementproject.urls'
