62e4335bce582959c0b78ca53c927db6321d1b87
MiyazakiHiroaki <h12.miyazakihiroaki@gmail.com>
2022-11-01 15:41:35 +0900

    1101

--- shift_managementproject/settings.py
+++ shift_managementproject/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shiftmanagementsystem.herokuapp.com']
 ALLOWED_HOSTS = ['*']
